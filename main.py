f0409a051a704c277cab8dc34443bde579c4f1b7
Kalmanis <Kalmanis@github.com>
2020-05-08 03:09:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ app = Flask('app')
 def index_page():
   return render_template ('BioM.html')
 
+@app.route('/BioCh')
+def BioCh():
+    return render_template('BioCh.html')
+    
+@app.route('/BioC')
+def BioC():
+    return render_template('BioC.html')
+
+@app.route('/BioR')
+def BioR():
+    return render_template('BioR.html')
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
